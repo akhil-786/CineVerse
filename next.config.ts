5bd3437b7821fd740f3101a6d8a2b80a7d33d421
Akhil <akhil49919@gmail.com>
2025-11-06 18:05:18 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -43,6 +44,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'img.flawlessfiles.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uk.static.webuy.com',
         port: '',
         pathname: '/**',
       }
